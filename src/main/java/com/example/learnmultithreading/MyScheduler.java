3bb6f831b09b4020a85a0fcdc51c6479beaf0a51
hemant097 <hem789ant1997@outlook.com>
2026-09-11 14:13:33 +0530

    cron added

--- src/main/java/com/example/learnmultithreading/MyScheduler.java
+++ src/main/java/com/example/learnmultithreading/MyScheduler.java
@@ -9,9 +9,38 @@ import org.springframework.stereotype.Component;
 public class MyScheduler {
 
     //it doesn't care whether the execution time takes 1sec, it starts every 2 sec
-    @Scheduled(fixedRate = 2000) //not concurrent, thus only 1 runs at a time
-    void logMe(){
-        log.info("Scheduler1 started ...{}",Thread.currentThread().getName());
+//    @Scheduled(fixedRate = 2000) //not concurrent, thus only 1 runs at a time
+//    void logMe(){
+//        log.info("Scheduler1 started ...{}",Thread.currentThread().getName());
+//
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        log.info("Scheduler1 ended ...{}",Thread.currentThread().getName());
+//
+//    }
+
+//    Exactly one of the 'cron', 'fixedDelay' or 'fixedRate' attributes is required
+//    @Scheduled(fixedDelay = 1000) //delays the task for 1sec after the end of one execution, and start of the next
+//    void logYou(){
+//        log.info("Scheduler2 started ...{}",Thread.currentThread().getName());
+//
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        log.info("Scheduler2 ended ...{}",Thread.currentThread().getName());
+//
+//    }
+
+    @Scheduled(cron = "*/5 * * * * *")
+    void logCron(){
+        log.info("Scheduler cron started ...{}",Thread.currentThread().getName());
 
         try {
             Thread.sleep(1000);
@@ -19,22 +48,7 @@ public class MyScheduler {
             throw new RuntimeException(e);
         }
 
-        log.info("Scheduler1 ended ...{}",Thread.currentThread().getName());
-
-    }
-
-//    Exactly one of the 'cron', 'fixedDelay' or 'fixedRate' attributes is required
-    @Scheduled(fixedDelay = 1000) //delays the task for 1sec after the end of one execution, and start of the next
-    void logYou(){
-        log.info("Scheduler2 started ...{}",Thread.currentThread().getName());
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        log.info("Scheduler2 ended ...{}",Thread.currentThread().getName());
+        log.info("Scheduler cron ended ...{}",Thread.currentThread().getName());
 
     }
 }
